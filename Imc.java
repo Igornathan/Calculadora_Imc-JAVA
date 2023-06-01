fb4f5459efa5c593a381320620a5a22b47a7cb92
Igor Nathan Da Silva Almeida <81446483+Igornathan@users.noreply.github.com>
2023-06-01 10:43:22 -0300

    Add files via upload

--- Imc.java
+++ Imc.java
@@ -24,18 +24,18 @@ public class Imc {
                 double imc=peso/(altura*altura);
                 System.out.println(imc);
                 
-            if(imc< 18.5){
+            if(imc< 18.5){   //ESTE IF VERIFICA QUEM ESTA ABAIXO DO PESO
                 System.out.println("Você está abaixo do peso");
-            }else if(imc >= 18.5 && imc <= 24.9){
+            }else if(imc >= 18.5 && imc <= 24.9){    //ESTE IF VERIFICA QUEM ESTA ABAIXO DO PESO
                 System.out.println("Você está no peso correto");
-            }else if(imc >= 25 && imc <= 29.9){
+            }else if(imc >= 25 && imc <= 29.9){      //ESTE IF VERIFICA QUEM ESTA NO PESO CORRETO
                 System.out.println("Você está acima do peso");
-            }else if(imc >= 30 && imc <= 34.9){
+            }else if(imc >= 30 && imc <= 34.9){      //ESTE IF VERIFICA QUEM ESTA ACIMA DO PESO
                 System.out.println("Você está obeso grau 1, cuidado");
-            }else if(imc >= 35 && imc <= 39.9){
+            }else if(imc >= 35 && imc <= 39.9){      //ESTE IF VERIFICA QUEM ESTA OBESO GRAU 1 
                 System.out.println("Você está obeso grau 2, corra.");
-            }else{
-                System.out.println("Você está obeso morbido.");
+            }else{                                   //ESTE IF VERIFICA QUEM ESTA OBESO GRAU 2
+                System.out.println("Você está obeso morbido.");   //ESTE IF VERIFICA QUEM ESTA OBESO MÓRBIDO
             }
     }
     
